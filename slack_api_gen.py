4d79655dea9d20a006fade16faedb9ba28cf7add
Shu Sugimoto <shu@su.gimo.to>
2018-07-26 14:41:47 +0900

    Make slack_api_gen.py consistent across environments

--- slack_api_gen.py
+++ slack_api_gen.py
@@ -1,23 +1,62 @@
+'''
+slack_api_gen.py
+Auto-generate st2 action meta yamls by scraping api.slack.com
+
+Usage: python slack_api_gen.py
+
+Make sure to have following packages in place to use this script.
+
+- pyyaml
+- bs4
+- html5lib
+'''
+
 import yaml
 import re
 import urllib2
 from bs4 import BeautifulSoup
+
+# Use OrderedDict to keep params order listed in API web page
+from collections import OrderedDict
+
+
+# pyyaml representer method for OrderedDict
+def represent_odict(dumper, instance):
+    return dumper.represent_mapping('tag:yaml.org,2002:map', instance.items())
+
+
+yaml.SafeDumper.add_representer(OrderedDict, represent_odict)
+
+
+# For fields that should be double-quoted in resulting yaml should use this
+# str wrapper class
+class doublequoted_string(str):
+    pass
+
+
+# pyyaml representer method for doublequoted_string class defined above
+def doublequoted_string_representer(dumper, data):
+    return dumper.represent_scalar(u'tag:yaml.org,2002:str', data, style='"')
+
+
+yaml.SafeDumper.add_representer(
+    doublequoted_string, doublequoted_string_representer)
 
 method_dict = {}
 base_url = 'https://api.slack.com/methods'
 
 api_doc_main = urllib2.urlopen('%s/channels.invite' % base_url)
 
-soup = BeautifulSoup(api_doc_main)
+soup = BeautifulSoup(api_doc_main, 'html5lib')
 
 api_methods = soup.find('select', id='api_method')
 
 for method in api_methods.stripped_strings:
     if method != 'View another method...':
-        method_dict[method] = {'params': {}}
+        method_dict[method] = {'params': OrderedDict()}
         method_url = "%s/%s" % (base_url, method)
         method_page = urllib2.urlopen(method_url)
-        method_soup = BeautifulSoup(method_page)
+        method_soup = BeautifulSoup(method_page, 'html5lib')
         method_description = method_soup.find('section', attrs={
             "class": "tab_pane selected clearfix large_bottom_padding"}) \
             .find_all('p')[0].text
@@ -46,30 +85,33 @@ for method in api_methods.stripped_strings:
 for method in method_dict:
 
     file_name = 'actions/%s.yaml' % method
-    output_dict = {'name': method,
-                   'runner_type': 'python-script',
-                   'enabled': True,
-                   'entry_point': 'run.py',
-                   'description': method_dict[method]['description'],
-                   'parameters': {
-                       'end_point': {
-                           'type': 'string',
-                           'immutable': True,
-                           'default': method}
-                   }
-                   }
+    output_dict = {
+        'name': method,
+        'runner_type': 'python-script',
+        'enabled': True,
+        'entry_point': 'run.py',
+        'description': doublequoted_string(method_dict[method]['description']),
+        'parameters': OrderedDict()}
+    output_dict['parameters']['end_point'] = {
+        'type': 'string',
+        'immutable': True,
+        'default': method}
 
     for param in method_dict[method]['params']:
         if param == 'token':
             method_dict[method]['params'][param]['required'] = False
-        output_dict['parameters'][param] = {'type': 'string'}
-        if method_dict[method]['params'][param]['default'] is not None:
-            output_dict['parameters'][param]['default'] = \
-                method_dict[method]['params'][param]['default']
+        output_dict['parameters'][param] = OrderedDict()
         output_dict['parameters'][param]['required'] = \
             method_dict[method]['params'][param]['required']
+        if method_dict[method]['params'][param]['default'] is not None:
+            output_dict['parameters'][param]['default'] = \
+                doublequoted_string(
+                    method_dict[method]['params'][param]['default'])
+        output_dict['parameters'][param]['type'] = 'string'
 
-    print yaml.safe_dump(output_dict, default_flow_style=False)
+    print yaml.safe_dump(
+        output_dict, default_flow_style=False, width=float('inf'))
     fh = open(file_name, 'w')
-    fh.write(yaml.safe_dump(output_dict, default_flow_style=False))
+    fh.write(yaml.safe_dump(
+        output_dict, default_flow_style=False, width=float('inf')))
     fh.close()
