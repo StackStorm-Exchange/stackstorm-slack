a298445bb0d70ab4414e459beb07aff367bc153c
Tomaz Muraus <tomaz@tomaz.me>
2020-04-14 19:09:07 +0200

    Various changes and improvements to the post_message action.
    - Make icon_emoji parameter optional.
    - Add new icon_url parameter.
    - Make post_message_action.webhook_url config optional.

--- actions/post_message.py
+++ actions/post_message.py
@@ -11,11 +11,12 @@ __all__ = [
 
 
 class PostMessageAction(Action):
-    def run(self, message, username=None, icon_emoji=None, channel=None,
+    def run(self, message, username=None, icon_emoji=None, icon_url=None, channel=None,
             disable_formatting=False, webhook_url=None):
         config = self.config.get('post_message_action', {})
         username = username if username else config['username']
         icon_emoji = icon_emoji if icon_emoji else config.get('icon_emoji', None)
+        icon_url = icon_url if icon_url else config.get('icon_url', None)
         channel = channel if channel else config.get('channel', None)
         webhook_url = webhook_url if webhook_url else config.get('webhook_url', None)
 
@@ -28,9 +29,14 @@ class PostMessageAction(Action):
         headers['Content-Type'] = 'application/x-www-form-urlencoded'
         body = {
             'username': username,
-            'icon_emoji': icon_emoji,
             'text': message
         }
+
+        if icon_emoji:
+            body['icon_emoji'] = icon_emoji
+
+        if icon_url:
+            body['icon_url'] = icon_url
 
         if channel:
             body['channel'] = channel
