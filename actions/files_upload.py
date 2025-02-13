1fdfe646764d8fff08fee515f18595da437a1b0a
Jacob Zufelt <jzufelt@adobe.com>
2025-02-13 14:50:19 -0700

    Migrate file.upload to slack_sdk helper

--- actions/files_upload.py
+++ actions/files_upload.py
@@ -1,11 +1,11 @@
-from run import SlackAction
+from st2common.runners.base_action import Action
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
-
-class FilesUploadAction(SlackAction):
-
+class FilesUploadAction(Action):
     def run(self, **kwargs):
-        # https://requests.readthedocs.io/en/master/user/quickstart/#post-a-multipart-encoded-file
-        files = None
+        token = kwargs.get('token') if kwargs.get('token') else self.config['action_token']
+        client = WebClient(token=token)
         if 'file_path' in kwargs and kwargs['file_path']:
             if 'file' in kwargs and kwargs['file']:
                 raise RuntimeError('Passing in "file" and "file_path" at the same time is'
@@ -14,10 +14,20 @@ class FilesUploadAction(SlackAction):
                                    ' then use the "file_path" parameter. Otherwise, if you'
                                    ' would like to read the file yourself and pass in the data'
                                    ' then use the "file" parameter set to the content.')
-            # the name 'file' is hard coded because that's the name of the parameter
-            # that the Slack API is expecting
-            files = {'file': open(kwargs['file_path'], 'rb')}
-            kwargs.pop('file', None)
-            kwargs.pop('file_path')
-
-        super(FilesUploadAction, self).run(files=files, **kwargs)
+        try:
+            file = kwargs['file'] if kwargs['file'] else kwargs['file_path']
+            response = client.files_upload_v2(
+                filename=kwargs['filename'],
+                file=file,
+                content=kwargs['content'],
+                title=kwargs['title'],
+                channel=kwargs['channels'],
+                initial_comment=kwargs['initial_comment'],
+                thread_ts=kwargs['thread_ts'],
+            )
+            assert response['file']
+            return response.get('file', {}).get('name')
+        except SlackApiError as e:
+            assert e.response["ok"] is False
+            assert e.response["error"]
+            return False, e.response['error']
